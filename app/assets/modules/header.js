1bca2dda30e763ed64b441cc1c5a1a0e9fb29e8a
bermuda-ut <hoso1312@gmail.com>
2017-12-25 16:43:06 +1100

    blog improvement

--- app/assets/modules/header.js
+++ app/assets/modules/header.js
@@ -1,7 +1,12 @@
-if(window.location.pathname === "/") {
-    document.getElementById("blog").classList.add("inverse");
-    document.getElementById("blog").setAttribute("href", "#");
-} else if(window.location.pathname.indexOf("about") >= 0) {
-    document.getElementById("about").classList.add("inverse");
-    document.getElementById("about").setAttribute("href", "#");
+let elem;
+
+if (window.location.pathname === "/") {
+    elem = document.getElementById("blog");
+
+} else if (window.location.pathname.indexOf("about") >= 0) {
+    elem = document.getElementById("about");
 }
+
+elem.classList.add("inverse");
+elem.classList.add("disabled");
+elem.setAttribute("href", "#");
