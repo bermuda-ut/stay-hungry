30c0193ea77b2783ee151d2aeb5557f2089917ea
sanmu <hoso1312@gmail.com>
2018-09-14 16:23:28 +1000

    Summary style polish

--- resume/src/component/Card.js
+++ resume/src/component/Card.js
@@ -12,8 +12,12 @@ class Card extends Component {
   }
 
   render() {
-    let descClass = this.state.hover ? "" : "opacity-0";
-    let iconClass = this.state.hover ? "opacity-0" : "";
+    let descStyle = {
+      opacity: this.state.hover ? "1" : "0",
+    }
+    let iconStyle = {
+      opacity: this.state.hover ? "0.07" : "1",
+    }
 
     return (
       <div className="Card"
@@ -21,16 +25,16 @@ class Card extends Component {
         onMouseLeave={() => this.setState({ hover: false })}>
 
         <div className="top">
-          <div className={`icon ${iconClass}`}>
+          <div className="icon" style={iconStyle}>
             {this.props.icon}
           </div>
 
-          <div className={`desc ${descClass}`}>
+          <div className="desc" style={descStyle}>
             {this.props.desc}
           </div>
         </div>
 
-        <h1>{this.props.title}</h1>
+        <h1 style={iconStyle}>{this.props.title}</h1>
       </div>
     );
   }
