f08879ee6148d3a959c311540b59d60bdf09ac4b
sanmu <hoso1312@gmail.com>
2018-09-14 11:00:53 +1000

    hackathon and programming experience layout

--- resume/src/HackatonAwards/HackatonAwards.js
+++ resume/src/HackatonAwards/HackatonAwards.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 
-import Card from '../component/Card';
+import codebrew from './codebrewaward.png';
+import startupwk from './startupweekendaward.png';
+import spaceapps from './spaceappsaward.png';
+import unihack from './unihackaward.png';
 
 class HackathonAwards extends Component {
   render() {
     return (
       <Grid container spacing={8}>
-        <p>HackathonAwards</p>
+        <Grid lg={3}>
+          <img src={codebrew} alt="CodeBrew 2016"/>
+        </Grid>
+
+        <Grid lg={3}>
+          <img src={startupwk} alt="Startup Weekend 2017"/>
+        </Grid>
+
+        <Grid lg={3}>
+          <img src={spaceapps} alt="Space Apps 2017"/>
+        </Grid>
+
+        <Grid lg={3}>
+          <img src={unihack} alt="UNIHACK 2016"/>
+        </Grid>
+
+        <Grid lg={6}>
+          <img src={unihack} alt="UNIHACK 2017 Mentor"/>
+        </Grid>
+
+        <Grid lg={6}>
+          <img src={unihack} alt="UNIHACK 2018 Judge / Sponsor Representative / Mentor"/>
+        </Grid>
       </Grid>
     );
   }
--- resume/src/ProgrammingExperience/ProgrammingExperience.js
+++ resume/src/ProgrammingExperience/ProgrammingExperience.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 
-import Card from '../component/Card';
-
 class ProgrammingExperience extends Component {
   render() {
     return (
       <Grid container spacing={8}>
-        <p>Programming and Framework Experience</p>
+        <Grid item xs={12}>
+          <div className="igetpaid">
+            <p>Python/ML/Tensorflow</p>
+            <p>Frontend/React</p>
+            <p>Backend/Javascript/Typescript/Node.js</p>
+            <p>Java/Spring</p>
+            <p>c#/gamedev/Unity</p>
+            <p>C/embedded system</p>
+            <p>Haskell/functional</p>
+            <p>Prolog/functional</p>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="confident">
+            <p>Java/Android App Development</p>
+            <p>App Development/React Native</p>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="playedaround">
+            <p>c++/gamedev/Unreal</p>
+            <p>Ruby on Rails</p>
+          </div>
+        </Grid>
       </Grid>
     );
   }
