faae0502536cbe9aaf97b483e07d357396807626
sanmu <hoso1312@gmail.com>
2018-09-14 13:58:58 +1000

    work experience content

--- resume/src/SummaryCards/SummaryCards.js
+++ resume/src/SummaryCards/SummaryCards.js
@@ -17,42 +17,42 @@ class SummaryCards extends Component {
         <Grid item xs={4}>
           <Card
             title="Agile to the Bones"
-            icon={<img src={agileIcon}/>}
+            icon={<img src={agileIcon} />}
             desc={<p>Long-time agile developer praticing sprint planning and self/team management. Skills amplified by solid discipline and organization skills.</p>}
           />
         </Grid>
         <Grid item xs={4}>
           <Card
             title="Node.js Minimalist"
-            icon={<img src={nodeIcon}/>}
+            icon={<img src={nodeIcon} />}
             desc={<p>Node for all - typical fullstack web projects, dev-ops tools and IoT gateways. My motto is minimal work (for both machines and hoomans) for maximum effect - in long term.</p>}
           />
         </Grid>
         <Grid item xs={4}>
           <Card
             title="AI and ML"
-            icon={<img src={aiIcon}/>}
-            desc={<p>Not-Just-A-Buzzword&trade;<br/>From analysing the problem space to development, I work closely with the customer to optimize their business with intelligent systems. This solution often includes advanced AI (ie. some fun graph traversals) and ML (ie. I find features, computer optimize model). Dev-ops and automation has huge emphasis in my solutions.</p>}
+            icon={<img src={aiIcon} />}
+            desc={<p>Not-Just-A-Buzzword&trade;<br />From analysing the problem space to development, I work closely with the customer to optimize their business with intelligent systems. This solution often includes advanced AI (ie. some fun graph traversals) and ML (ie. I find features, computer optimize model). Dev-ops and automation has huge emphasis in my solutions.</p>}
           />
         </Grid>
         <Grid item xs={6}>
           <Card
             title="Code that scales in Prod and Tests"
-            icon={<img src={designIcon}/>}
+            icon={<img src={designIcon} />}
             desc={<p>In architecture design, automated and large-scale testing is one of the priorities. Functional-OO hybrid design is practiced in my software design, keeping thing minimal (easy to understand) or scalable when required.</p>}
           />
         </Grid>
         <Grid item xs={6}>
           <Card
             title="Hackathon Enthusiast"
-            icon={<img src={hackIcon}/>}
+            icon={<img src={hackIcon} />}
             desc={<p>Participated in countless hackathons? Check. Won 4 hackathons? Check. Mentored in hackathons? Check. Hackathon Judge and Sponsor Representative? Check. Passion and Love for next-generation of hackers? CHECK!</p>}
           />
         </Grid>
         <Grid item xs={12}>
           <Card
             title="Work rights in AU and KR"
-            icon={<img src={workRightIcon}/>}
+            icon={<img src={workRightIcon} />}
             desc={<p>Work rights in Australia until 2020 and will be applying for Permament Residence at the end of the 2018 with 70 points. Holding citizenship of South Korea :)</p>}
           />
         </Grid>
--- resume/src/WorkExperience/WorkExperience.js
+++ resume/src/WorkExperience/WorkExperience.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
+
+import boschLogo from './bosch.png';
+import unimelbLogo from './unimelb.svg';
+import clearbridgeLogo from './clearbridge.png';
 
 const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 function dateFormat(d) {
@@ -21,7 +24,7 @@ class Experience extends Component {
   render() {
     return (
       <Grid item xs={12}>
-        <ExpansionPanel expanded={this.state.expanded} onChange={() => this.setState({expanded: !this.state.expanded})}>
+        <ExpansionPanel expanded={this.state.expanded} onChange={() => this.setState({ expanded: !this.state.expanded })}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <h1>{this.props.title}</h1>
             <p>{`${dateFormat(this.props.startDate)} - ${this.props.endDate ? dateFormat(this.props.endDate) : "current"}`}</p>
@@ -37,14 +40,61 @@ class Experience extends Component {
 
 class ExperienceGroup extends Component {
   render() {
-    return(
+    return (
       <Grid container item xs={12}>
         <Grid item xs={3}>
-          {this.props.icon}
-          {this.props.company}
+          <a href={this.props.url} target="_blank">
+            {this.props.icon}
+          </a>
         </Grid>
         <Grid item xs={9}>
           {this.props.children}
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+class SkillGauge extends Component {
+  render() {
+    return (
+      <Grid container item xs={12}>
+        <Grid xs={4}>
+          {this.props.skill}
+        </Grid>
+        <Grid xs={8}>
+          {this.props.score}
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+const SUBJECT_NAME = {
+  "comp30026": "Models of Computation",
+  "comp30024": "Artificial Intelligence",
+  "comp30023": "Computing Systems",
+  "swen30006": "Software Modelling and Design",
+  "comp20005": "Engineering Computation",
+  "comp10001": "Foundations of Computing",
+  "comp90059": "Introduction to Python Programming",
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <Grid container item xs={12}>
+        <Grid xs={4}>
+          <p>
+            {this.props.year}, Sem {this.props.semester}
+          </p>
+        </Grid>
+        <Grid xs={8}>
+          <p>
+            <a href={`https://handbook.unimelb.edu.au/${this.props.year}/subjects/${this.props.code}`} target="_blank">
+              {this.props.subject ? this.props.subject : SUBJECT_NAME[this.props.code]}
+            </a>
+          </p>
         </Grid>
       </Grid>
     )
@@ -55,40 +105,60 @@ class WorkExperience extends Component {
   render() {
     return (
       <Grid container spacing={8}>
-        <ExperienceGroup icon={<i>TestIcon</i>} company="Bosch Australia">
+        <ExperienceGroup icon={<img src={boschLogo} alt="Bosch Australia" />} url="https://www.bosch.com.au/" company="Bosch Australia">
           <Experience
             title="IoT Software Engineer"
-            desc={<p>Wow much work</p>}
+            desc={<p>I work end-to-end: from customer's ideas to product development &amp; deployment.
+My projects involve developing wide range of software - analytics, machine learning, information retrieval, web fullstack, device platform, libraries, computer games and firmware.
+As an Agile developer, I work on formulating business strategy and goals, product development, requirements documentation, designing software and infrastructure, and more.
+</p>}
             startDate={new Date("Dec 2017")}
-            endDate={undefined}/>
+            endDate={undefined} />
 
           <Experience
             title="Student IoT Engineer"
-            desc={<p>Wow much work</p>}
+            desc={<p>I do it all - overall software solution &amp; architecture design, Dev-Ops, Git hooks and extensions, responsive web application development, API design and development, database design, embedded systems (FreeRTOS), CAN bus debugging, reverse-engineering and simulations.
+              Also worked in setting up business-goal-oriented product demos for Bosch products at conventions.</p>}
             startDate={new Date("Jan 2017")}
-            endDate={new Date("Dec 2017")}/>
+            endDate={new Date("Dec 2017")} />
         </ExperienceGroup>
 
-        <ExperienceGroup icon={<i>TestIcon</i>} company="University of Melbourne">
+        <ExperienceGroup icon={<img src={unimelbLogo} alt="University of Melbourne" style={{ backgroundColor: "rgb(9, 65, 131)", padding: "2em" }} />} company="University of Melbourne">
           <Experience
-            title="Tutor"
-            desc={<p>Wow much work</p>}
+            title="Tutor and Demonstrator"
+            desc={<div>
+              <h3>Anonymous Student Feedback</h3>
+              <div>
+                <SkillGauge skill="Organization Skills" score={4.9 / 5} />
+                <SkillGauge skill="Clear and Understandable Explanation" score={4.9 / 5} />
+                <SkillGauge skill="Encouraging Particpation" score={4.7 / 5} />
+                <SkillGauge skill="Gauge Understanding" score={4.6 / 5} />
+              </div>
+              <h3>History</h3>
+              <Subject year={2018} semester={2} code="comp30026"/>
+              <Subject year={2018} semester={1} code="comp30024"/>
+              <Subject year={2018} semester={1} code="comp30023"/>
+              <Subject year={2018} semester={1} code="comp20005"/>
+              <Subject year={2018} semester={1} code="swen30006"/>
+              <Subject year={2018} semester="Summer" code="comp90059"/>
+              <Subject year={2016} semester={1} code="comp10001"/>
+            </div>}
             startDate={new Date("Feb 2016")}
-            endDate={undefined}/>
+            endDate={undefined} />
         </ExperienceGroup>
 
-        <ExperienceGroup icon={<i>TestIcon</i>} company="Clear Bridge Group">
+        <ExperienceGroup icon={<img src={clearbridgeLogo} alt="Clear Bridge Group"/>} url="http://www.clearbridge.com.au">
           <Experience
             title="Fullstack Developer"
-            desc={<p>Wow much work</p>}
+            desc={<p>Business Process Management System development using Meteor.js and Typescript. The application also acted as a friendly web-interface for jBPM. A combination of Blaze and React were used on the view side.</p>}
             startDate={new Date("Jan 2016")}
-            endDate={new Date("Dec 2015")}/>
+            endDate={new Date("Dec 2015")} />
 
           <Experience
             title="IT and Marketing Assistant"
-            desc={<p>Wow much work</p>}
+            desc={<p>Casual position to create a web shop using Magento framework. Customized Magento themes, scripts and extensions (PHP and JS), with a bit of WordPress and UI design. Duties also included classic Magento back-end work such as database migration and product management.</p>}
             startDate={new Date("Dec 2015")}
-            endDate={new Date("July 2015")}/>
+            endDate={new Date("July 2015")} />
         </ExperienceGroup>
 
       </Grid>
