e283d5e8db275077a769f816f312ae0d040a13e7
sanmu <hoso1312@gmail.com>
2018-09-14 14:49:38 +1000

    content for framework and programming experience

--- resume/src/ProgrammingExperience/ProgrammingExperience.js
+++ resume/src/ProgrammingExperience/ProgrammingExperience.js
@@ -1,33 +1,64 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 
+import android from './android.svg';
+import haskell from './haskell.svg';
+import machine from './machine.svg';
+import phone from './phone.svg';
+import rails from './rails.svg';
+import react from './react.svg';
+import node from './node.svg';
+import spring from './spring.svg';
+import tensorflow from './tensorflow.svg';
+import terminal from './terminal.svg';
+import unity from './unity.png';
+import unreal from './unreal.svg';
+
+class Framework extends Component {
+  render() {
+    return (
+      <Grid item xs={this.props.size ? this.props.size : 3}>
+        <div>
+          {this.props.logo}
+          <p>
+            {this.props.name}
+          </p>
+        </div>
+      </Grid>
+    )
+  }
+}
+
 class ProgrammingExperience extends Component {
   render() {
     return (
       <Grid container spacing={8}>
-        <Grid item xs={12}>
-          <div className="igetpaid">
-            <p>Python/ML/Tensorflow</p>
-            <p>Frontend/React</p>
-            <p>Backend/Javascript/Typescript/Node.js</p>
-            <p>Java/Spring</p>
-            <p>c#/gamedev/Unity</p>
-            <p>C/embedded system</p>
-            <p>Haskell/functional</p>
-            <p>Prolog/functional</p>
-          </div>
+        <Grid container item xs={12}>
+          <Grid item xs={12}>
+            <h2>I get Paid to do</h2>
+          </Grid>
+          <Framework logo={<img src={tensorflow} />} />
+          <Framework logo={<img src={react} />} />
+          <Framework logo={<img src={node} />} />
+          <Framework logo={<img src={spring} />} />
+          <Framework logo={<img src={unity} />} />
+          <Framework logo={<img src={machine} name="C/embedded system" />} />
+          <Framework logo={<img src={terminal} name="Bash Script" />} />
         </Grid>
-        <Grid item xs={12}>
-          <div className="confident">
-            <p>Java/Android App Development</p>
-            <p>App Development/React Native</p>
-          </div>
+        <Grid container item xs={12}>
+          <Grid item xs={12}>
+            <h2>I am confident with</h2>
+          </Grid>
+          <Framework logo={<img src={haskell} />} />
+          <Framework logo={<img src={phone} name="React Native" />} />
+          <Framework logo={<img src={android} />} />
         </Grid>
-        <Grid item xs={12}>
-          <div className="playedaround">
-            <p>c++/gamedev/Unreal</p>
-            <p>Ruby on Rails</p>
-          </div>
+        <Grid container item xs={12}>
+          <Grid item xs={12}>
+            <h2>I have played around with</h2>
+          </Grid>
+          <Framework logo={<img src={unreal} />} />
+          <Framework logo={<img src={rails} />} />
         </Grid>
       </Grid>
     );
